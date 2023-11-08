ac734a1e2b52b6752b7c2c4b1cd92bf0dca7e3ac
sidneyhenriquedev <sidneyhenrique.dev@gmail.com>
2023-11-08 17:35:32 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# quantos-graus
+### Como Usar
+
+Este é um aplicativo simples para verificar a temperatura de acordo com sua cidade ou pais digitado na Busca.
+
+1. Clone este repositório em sua máquina local usando o seguinte comando: git clone https://github.com/sidneyhenriquedev/quantos-graus
+
+2. Navegue até o diretório do projeto:
+    
+4. Abra o arquivo `index.html` em seu navegador da web.
+
+5. No aplicativo da web, insira o nome da cidade ou pais que queira verificar a temperatura do tempo e clique na lupa para pesquisar.
+
+6. O resultado será exibido no aplicativo da web.
+
+## Demonstração
+
+
+
+
+<img src="../img/quantos-graus.gif" alt="teste"/>
+
+## Notas
+
+Certifique-se de ter uma conexão com a Internet ao carregar o aplicativo, pois ele utiliza api de consulta de tempo.
+
+## Contribuição
+
+Contribuições são sempre bem-vindas! Se você deseja melhorar este projeto, siga estas etapas:
+
+1. Fork este repositório.
+2. Crie um branch: `git checkout -b feature/feature_desejada`.
+3. Faça suas alterações e faça commit delas: `git commit -m 'Adicione sua mensagem aqui'`.
+4. Envie para o branch original: `git push origin feature/feature_desejada`.
+5. Crie a solicitação de pull.
+
+## Licença
+
+Este projeto possui licença gratuita.
+
---
+++ js/scripts.js
@@ -0,0 +1,90 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKey = 'dc7b74fca1546b1655ddcd5205631f17';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}&lang=pt_br`)
+        .then(response => response.json())
+        .then(data => {
+
+            if (data.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (data.weather[0].main) {
+                //sky clean no rain very sun
+                case 'Clear':
+                    image.src = '/img/clear.png';
+                    document.getElementById('background').style.backgroundImage = "URL('tecklab-temperatura/img/sol.jpg')"
+                    document.getElementById('background').style.backgroundRepeat = 'no-repat'
+                    document.getElementById('background').style.backgroundSize = 'cover'
+                    break;
+                // is rain
+                case 'Rain':
+                    image.src = '/img/rain.png';
+                    document.getElementById('background').style.backgroundImage = "URL('tecklab-temperatura/img/chuva.jpg')"
+                    document.getElementById('background').style.backgroundRepeat = 'no-repat'
+                    document.getElementById('background').style.backgroundSize = 'cover'
+                    break;
+
+                case 'Snow':
+                    image.src = '/img/snow.png';
+
+                    document.getElementById('background').style.backgroundImage = "URL('tecklab-temperatura/img/neve.jpg')"
+                    document.getElementById('background').style.backgroundRepeat = 'no-repat'
+                    document.getElementById('background').style.backgroundSize = 'cover'
+                    break;
+
+                case 'Clouds':
+                    image.src = '/img/cloud.png';
+                    document.getElementById('background').style.backgroundImage = "URL('tecklab-temperatura/img/nublado.jpeg')"
+                    document.getElementById('background').style.backgroundRepeat = 'no-repat'
+                    document.getElementById('background').style.backgroundSize = 'cover'
+                    break;
+
+                case 'Haze':
+                    image.src = '/img/mist.png';
+                    break;
+
+                default:
+                    image.src = '';
+            }
+
+            temperature.innerHTML = `${parseInt(data.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${data.weather[0].description}`;
+            humidity.innerHTML = `${data.main.humidity}%`;
+            wind.innerHTML = `${parseInt(data.wind.speed)}Km/h`;
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+
+
+        });
+
+
+});
